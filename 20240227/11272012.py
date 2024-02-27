5192342470e8b1773991a26fd4de28aa3bea531a
林昱維 <wayne010104@gmail.com>
2024-02-27 20:16:21 +0800

    第三堂課

--- 20240227/11272012.py
+++ 20240227/11272012.py
@@ -1,1 +1,29 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+import feedparser
+
+# 設定要爬取的網址
+url = "https://news.pts.org.tw/xml/newsfeed.xml"
+
+# 使用 feedparser 解析 RSS feed
+feed = feedparser.parse(url)
+
+# 輸出 feed 的標題和項目數量
+print("Feed title:", feed.feed.title)
+print("Number of entries:", len(feed.entries))
+
+# 遍歷每一個 'entry'
+for entry in feed.entries:
+    # 輸出該項目的標題
+    print(entry.title)
+
+    #印出 summary
+    print(entry.summary)
+
+    #印出 link 
+    print(entry.link)
+
+    #印出 分隔線
+    print("-" * 80)
+    
+
